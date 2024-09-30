96275d16842a1dadbf5b927cfd58b42034549736
rogan <justsomegiraffes@gmail.com>
2024-09-30 11:57:05 -0400

    remove dependencies

--- FirstPersonDeath/Patches/KillPlayerPatch.cs
+++ FirstPersonDeath/Patches/KillPlayerPatch.cs
@@ -1,11 +1,8 @@
 ﻿using GameNetcodeStuff;
 using HarmonyLib;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FirstPersonDeath.Patches
 {
